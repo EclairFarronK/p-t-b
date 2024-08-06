6b0b1e98b16e623babf3f98fece02db48efbcf6c
tsp <3511875744@qq.com>
2024-08-07 00:43:40 +0800

    base

--- dev_tsp/test/test.py
+++ dev_tsp/test/test.py
@@ -1,7 +1,5 @@
 import base64
 
-import magic
-
 encoded_data = 'ASgb01ToD1A5+tPBbt603ce/T0oMgUADoKQx0nJA70AAcUwNk7j17Cguc0AMUkggmngDjP3aiXsakbjqOKBiE4Bx3pm6nFhxgUwlc0ACE9RSmTPy5oooAiDgdDknpTCwBxyaKKQz'
 
 if __name__ == '__main__':
@@ -10,7 +8,3 @@ if __name__ == '__main__':
     file_path = 'output_file'
     with open(file_path, 'wb') as file:
         file.write(decoded_data)
-
-    mime = magic.Magic()
-    file_type = mime.from_file('output_file')
-    print(file_type)
