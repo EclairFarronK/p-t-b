2b0d3af7e410cf9f3f6c912a3d24459c5fcfc50f
tsp <12345@qq.com>
2024-08-09 14:57:21 +0800

    ...

--- dev_tsp/telethon/message02.py
+++ dev_tsp/telethon/message02.py
@@ -6,19 +6,27 @@ db = mongoClient['telegram_db']
 
 
 async def main():
-    # client.get_dialogs()
-    # async for dialog in client.iter_dialogs(limit=None):
-    #     print(dialog.name, 'has ID', dialog.id)
-
     # You can print the message history of any chat:
     # client.get_messages('5768415700')
     async with client:
+        # client.get_dialogs()
+        # async for dialog in client.iter_dialogs(limit=None):
+        #     print(dialog.name, 'has ID', dialog.id)
+
         entity = 5768415700
         async for message in client.iter_messages(entity=entity, limit=None):
+            print(message.to_json())
+            print(message.from_id)
+            print(message.text)
             # 在哪个dialog，id，text，link
             # todo 有link了直接跳转过去，好像
-            message_json = {"dialog_id": entity, "id": message.id, "text": message.text, }
-            db.dialog.insert_one(message_json)
+            # todo 提高效率，先批量保存到内存中，然后批量存到MongoDB
+            message_json = {"entity": entity,
+                            "id": message.id,
+                            # rguo1
+                            "from_id": message.from_id,
+                            "text": message.text, }
+            # db.dialog.insert_one(message_json)
 
         await client.run_until_disconnected()
 
